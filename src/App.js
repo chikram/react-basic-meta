eac77088570e9d7580be461881b02691b7dd8ecc
Ikram Aslam <ikramaslam1020@gmail.com>
2023-08-28 17:20:32 +0500

    Exporting component in app file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Nav from "./component/Nav"
+import Promo from "./component/Promo"
+import Intro1 from "./component/Intro1"
+import Intro2 from "./component/Intro2"
+import Intro3 from "./component/Intro3"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Nav />
+      <Promo />
+      <Intro1 />
+      <Intro2 />
+      <Intro3 />
+    </>
+  )
 }
 
-export default App;
+export default App
