4612cd2b88b68a4eccdb1d31911e75494c5e17e7
Ikram Aslam <ikramaslam1020@gmail.com>
2023-08-28 19:34:26 +0500

    ProbPassing in react

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Promo from "./component/Promo"
 import Intro1 from "./component/Intro1"
 import Intro2 from "./component/Intro2"
 import Intro3 from "./component/Intro3"
+import ProbPassing from "./component/ProbPassing"
 function App() {
   return (
     <>
-      <Nav />
+      {/* <Nav />
       <Promo />
       <Intro1 />
       <Intro2 />
-      <Intro3 />
+      <Intro3 />*/}
+      <ProbPassing title="This is Prob" />
     </>
   )
 }
---
+++ src/component/ProbPassing.js
@@ -0,0 +1,6 @@
+function ProbPassing(probs) {
+    return (
+        <h1>{probs.title}</h1>
+    )
+}
+export default ProbPassing;
