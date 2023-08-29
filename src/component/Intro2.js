6b8f847f656919935464c19d16d5b8b2f0aff1e1
Ikram Aslam <ikramaslam1020@gmail.com>
2023-08-29 10:16:33 +0500

    inline and internal styling in react

--- src/component/Intro2.js
+++ src/component/Intro2.js
@@ -1,9 +1,15 @@
 function Intro2() {
+    const styles = {
+        color: "green",
+        padding: "10px",
+        fontWeight: "bold"
+    }
     return (
         <div className="blog-post-intro">
-            <h2>Why I love front-end web development</h2>
+            {/*inline css in react*/}
+            <h2 style={{ color: "red", fontFamily: "serif" }}>Why I love front-end web development</h2>
             <div>
-                <p>In this blog post, I'll list 10 reasons why I love to work as a front-end developer.</p>
+                <p style={styles}>In this blog post, I'll list 10 reasons why I love to work as a front-end developer.</p>
                 <p className="link">Read more...</p>
             </div>
         </div>
