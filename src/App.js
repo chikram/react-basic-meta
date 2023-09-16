36e2a66c26e3420a4d322acf2b8d7958d2ab27c2
Ikram Aslam <ikramaslam1020@gmail.com>
2023-09-16 15:34:47 +0500

    lab1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import Intro1 from "./component/Intro1"
 import Intro2 from "./component/Intro2"
 import Intro3 from "./component/Intro3"
 import ProbPassing from "./component/ProbPassing"
+function handleClick() {
+  let randomNum = Math.floor(Math.random() * 3) + 1
+  console.log(randomNum)
+  let userInput = prompt('type a number');
+  alert(`Computer number: ${randomNum}, Your guess: ${userInput}`)
+}
 function App() {
   return (
     <>
@@ -11,8 +17,13 @@ function App() {
       <Promo />
       <Intro1 />
       <Intro2 />
-      <Intro3 />*/}
-      <ProbPassing title="This is Prob" />
+      <Intro3 />
+       <ProbPassing title="This is Prob" />*/}
+      <div>
+        <h1>Task: Add a button and handle a click event</h1>
+        <button onClick={handleClick}>Guess the number between 1 and 3</button>
+      </div>
+
     </>
   )
 }
