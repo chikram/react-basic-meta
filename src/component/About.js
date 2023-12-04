388b8634bdbf75c2d37c9f9073fed53eed33b166
Ikram Aslam <ikramaslam1020@gmail.com>
2023-12-04 14:13:13 +0500

    setting routes in react

---
+++ src/component/About.js
@@ -0,0 +1,9 @@
+function About() {
+    return (
+        <div className="copyright">
+            <p>About us</p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/component/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div className="copyright">
+            <p>Made with love by Myself</p>
+        </div>
+    );
+};
+
+export default Home;
